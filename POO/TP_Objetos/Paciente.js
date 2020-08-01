a890b69c732da24318f24d7db660db42847e57f9
viviblasco <viviblasco@hotmail.com>
2020-08-01 12:51:59 -0300

    Create Paciente.js

---
+++ POO/TP_Objetos/Paciente.js
@@ -0,0 +1,8 @@
+var Paciente = /** @class */ (function () {
+    function Paciente(nombreYApellido, edad, telefono) {
+        this.nombreYApellido = nombreYApellido;
+        this.edad = edad;
+        this.telefono = telefono;
+    }
+    return Paciente;
+}());
