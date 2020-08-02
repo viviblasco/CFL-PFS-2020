d143ad064a928f3db069be56a41ef4a56aa4958f
viviblasco <viviblasco@hotmail.com>
2020-08-02 20:47:19 -0300

    Update gestorpacientes.js

--- POO/TP_Objetos/gestorpacientes.js
+++ POO/TP_Objetos/gestorpacientes.js
@@ -7,15 +7,11 @@ var GestorPacientes = /** @class */ (function () {
         this.contadorId = 0;
         this.lector = new LectorArchivos_1["default"]();
         if (pacienteArchivo) {
-            this.paciente = this.cargarPaciente(PacienteArchivo);
+            this.paciente = this.cargarPacientes(pacienteArchivo);
         }
         else {
             this.paciente = new Array();
         }
-        /*
-         * Carga de pacientes al gestor a partir de un archivo de texto
-         * se lee la ruta del archivo
-         */
     }
     /*
      * Carga de pacientes al gestor a partir de un archivo de texto
@@ -33,7 +29,7 @@ var GestorPacientes = /** @class */ (function () {
         for (var i = 0; i < pacienteString.length; i++) {
             aux = pacienteString[i].split(',');
             this.contadorId++;
-            var paciente = new Paciente_1["default"](this.contadorId, aux[0], aux[1], aux[2], parseInt(aux[3]));
+            var paciente = new Paciente_1["default"](this.contadorId, (aux[0]), parseInt(aux[1]), (aux[2]), parseInt(aux[3]), parseInt(aux[4]));
             pacienteArreglo.push(paciente);
         }
         return pacienteArreglo;
@@ -45,52 +41,53 @@ var GestorPacientes = /** @class */ (function () {
     GestorPacientes.prototype.buscarPaciente = function (paciente) {
         var indice = -1;
         for (var i = 0; i < this.paciente.length; i++) {
-            if (this.paciente[i].getDNI() === paciente.getDNI()
-                && this.paciente[i].getNombreYApellido() === paciente.getNombreYApellido()) {
+            if (this.paciente[i].getId() == paciente.getId()) {
                 indice = i;
             }
+            console.log(this.paciente[i].getId());
         }
         ;
         return indice;
     };
     //muestra el paciente buscado por nombre y apellido
-    GestorPacientes.prototype.verPacientePorNombre = function (NombreYApellido) {
+    GestorPacientes.prototype.verPacientePorNombre = function (nombreYApellido) {
         for (var i = 0; i < this.paciente.length; i++) {
-            if (this.paciente[i].getNombreYApellido() === NombreYApellido) {
+            if (this.paciente[i].getNombreYApellido() === nombreYApellido) {
                 console.log(this.paciente[i]);
             }
         }
         ;
     };
-    //muestra el paciente buscado por DNI
-    GestorPacientes.prototype.verPacientePorDni = function (DNI) {
+    //muestra el paciente buscado por el doctor que lo atiende
+    GestorPacientes.prototype.verPacientePorDoctor = function (doctor) {
         for (var i = 0; i < this.paciente.length; i++) {
-            if (this.paciente[i].getDNI() === DNI) {
+            if (this.paciente[i].getDoctor() == doctor) {
                 console.log(this.paciente[i]);
             }
         }
         ;
     };
     //agrega un paciente a la lista
-    GestorPacientes.prototype.agregarPaciente = function (idPaciente, DNI, nombreYApellido, edad, telefono) {
+    GestorPacientes.prototype.agregarPaciente = function (doctor, DNI, nombreYApellido, edad, telefono) {
         this.contadorId++;
-        var paciente = new Paciente_1["default"](this.contadorId, idPaciente, DNI, nombreYApellido, edad, telefono);
+        var paciente = new Paciente_1["default"](this.contadorId, doctor, DNI, nombreYApellido, edad, telefono);
         this.paciente.push(paciente);
+        console.log("la/el paciente quedo agregada al registro");
     };
-    //busca un paciente por id en la lista de registro de pacientes
-    GestorPacientes.prototype.buscarPacientePorId = function (idPaciente) {
+    //busca un paciente por id en la lista de registro de pacientes2
+    GestorPacientes.prototype.buscarPacientePorId = function (id) {
         var indice = -1;
         for (var i = 0; i < this.paciente.length; i++) {
             var aux = this.paciente[i];
-            if (aux.getIdPaciente() === idPaciente) {
+            if (aux.getId() === id) {
                 indice = i;
             }
         }
         return indice;
     };
     //borrar un paciente del registro
-    GestorPacientes.prototype.borrarPaciente = function (idPaciente) {
-        var indice = this.buscarPacientePorId(idPaciente);
+    GestorPacientes.prototype.borrarPaciente = function (id) {
+        var indice = this.buscarPacientePorId(id);
         if (indice === -1) {
             console.log("El paciente no esta registrado");
         }
@@ -100,26 +97,29 @@ var GestorPacientes = /** @class */ (function () {
         }
     };
     //modifica los datos de un paciente en el registro
-    GestorPacientes.prototype.modificarPaciente = function (idPaciente, DNI, nombreYApellido, edad, telefono) {
-        var indice = this.buscarPacientePorId(idPaciente);
+    GestorPacientes.prototype.modificarPaciente = function (id, doctor, Dni, nombreYApellido, edad, telefono) {
+        var indice = this.buscarPacientePorId(id);
         if (indice === -1) {
             console.log("El paciente no esta registrado");
         }
         else {
-            if (idPaciente) {
-                this.paciente[indice].setAutor(idPaciente);
+            if (id) {
+                this.paciente[indice].setId(id);
             }
-            if (DNI) {
-                this.paciente[indice].setTitulo(DNI);
+            if (doctor) {
+                this.paciente[indice].setDoctor(doctor);
+            }
+            if (Dni) {
+                this.paciente[indice].setDni(Dni);
             }
             if (nombreYApellido) {
-                this.paciente[indice].setEditorial(nombreYApellido);
+                this.paciente[indice].setNombreYApellido(nombreYApellido);
             }
             if (edad) {
-                this.paciente[indice].setEdicion(edad);
+                this.paciente[indice].setEdad(edad);
             }
             if (telefono) {
-                this.paciente[indice].setEdicion(telefono);
+                this.paciente[indice].setTelefono(telefono);
             }
             console.log("El registro del paciente quedo actualizado");
         }
