f75f3ace6d079f6af628cfe086f56d07187b8c6e
viviblasco <viviblasco@hotmail.com>
2020-08-02 20:47:24 -0300

    Update GestorPacientes.ts

--- POO/TP_Objetos/GestorPacientes.ts
+++ POO/TP_Objetos/GestorPacientes.ts
@@ -36,7 +36,7 @@ export default class GestorPacientes {
         for (let i: number = 0; i<pacienteString.length; i++) {
             aux = pacienteString[i].split(',');
             this.contadorId ++;
-            let paciente = new Paciente(this.contadorId, aux[0], aux[1], aux[2], parseInt(aux[3]));
+            let paciente = new Paciente(this.contadorId,(aux[0]), parseInt(aux[1]), (aux[2]), parseInt(aux[3]), parseInt(aux[4]));
             pacienteArreglo.push(paciente);
         }
 
@@ -49,43 +49,46 @@ export default class GestorPacientes {
     public buscarPaciente(paciente: Paciente): number {
         let indice = -1;
         for(let i: number = 0; i<this.paciente.length; i++) {
-            if (this.paciente[i].getDNI() === paciente.getDNI() 
-            && this.paciente[i].getNombreYApellido() === paciente.getNombreYApellido() ) {
+            if (this.paciente[i].getId() == paciente.getId() ) {
                 indice = i;
             }
+            console.log(this.paciente[i].getId())
         };
         return indice;
     }
+
+
         //muestra el paciente buscado por nombre y apellido
-        public verPacientePorNombre(NombreYApellido: string): void {
+        public verPacientePorNombre(nombreYApellido: string): void {
             for(let i: number = 0; i<this.paciente.length; i++) {
-                if (this.paciente[i].getNombreYApellido() === NombreYApellido) {
+                if (this.paciente[i].getNombreYApellido() === nombreYApellido) {
                     console.log(this.paciente[i]);
                 }
             };
         }
-        //muestra el paciente buscado por DNI
+        //muestra el paciente buscado por el doctor que lo atiende
 
-        public verPacientePorDni(DNI: number): void {
+        public verPacientePorDoctor(doctor: string): void {
             for(let i: number = 0; i<this.paciente.length; i++) {
-                if (this.paciente[i].getDNI() === DNI) {
+                if (this.paciente[i].getDoctor() == doctor) {
                     console.log(this.paciente[i]);
                 }
             };
         }
         //agrega un paciente a la lista
-        public agregarPaciente(idPaciente:number,DNI:number,nombreYApellido:string,edad:number,telefono:number): void {
+        public agregarPaciente(doctor:string,DNI:number,nombreYApellido:string,edad:number,telefono:number): void {
             this.contadorId++;
-            let paciente: Paciente= new Paciente(this.contadorId, idPaciente, DNI,nombreYApellido, edad, telefono );
+            let paciente: Paciente= new Paciente(this.contadorId, doctor, DNI,nombreYApellido, edad, telefono );
             this.paciente.push(paciente);
+            console.log("la/el paciente quedo agregada al registro");
         }
 
-        //busca un paciente por id en la lista de registro de pacientes
-        private buscarPacientePorId(idPaciente: number): number {
+        //busca un paciente por id en la lista de registro de pacientes2
+        private buscarPacientePorId(id: number): number {
             let indice = -1;
             for(let i: number = 0; i<this.paciente.length; i++) {
                 let aux: Paciente = this.paciente[i];
-                if (aux.getIdPaciente() === idPaciente) {
+                if (aux.getId() === id) {
                     indice = i;
                 } 
             }
@@ -94,8 +97,8 @@ export default class GestorPacientes {
 
 
         //borrar un paciente del registro
-        public borrarPaciente(idPaciente: number): void {
-            let indice: number = this.buscarPacientePorId(idPaciente);
+        public borrarPaciente(id: number): void {
+            let indice: number = this.buscarPacientePorId(id);
             if ( indice === -1) {
                 console.log("El paciente no esta registrado");
             } else {
@@ -106,25 +109,29 @@ export default class GestorPacientes {
 
         //modifica los datos de un paciente en el registro
         
-        public modificarPaciente(idPaciente:number,DNI:number,nombreYApellido:string,edad:number,telefono:number): void {
-        let indice: number = this.buscarPacientePorId(idPaciente);
+        public modificarPaciente(id:number,doctor:string,Dni:number,nombreYApellido:string,edad:number,telefono:number): void {
+        let indice: number = this.buscarPacientePorId(id);
         if ( indice === -1) {
             console.log("El paciente no esta registrado");
         } else {
-            if (idPaciente) {
-                this.paciente[indice].setAutor(idPaciente);
+            if (id) {
+                this.paciente[indice].setId(id);
             }
-            if (DNI) {
-                this.paciente[indice].setTitulo(DNI);
+            if (doctor) {
+                this.paciente[indice].setDoctor(doctor);
+            }
+
+            if (Dni) {
+                this.paciente[indice].setDni(Dni);
             }
             if (nombreYApellido) {
-                this.paciente[indice].setEditorial(nombreYApellido);
+                this.paciente[indice].setNombreYApellido(nombreYApellido);
             }
             if (edad) {
-                this.paciente[indice].setEdicion(edad);
+                this.paciente[indice].setEdad(edad);
             }
             if (telefono) {
-                this.paciente[indice].setEdicion(telefono);
+                this.paciente[indice].setTelefono(telefono);
             }
             console.log("El registro del paciente quedo actualizado");
         }
