c84a24e4389914d52cb3445ade578722b0c4ecf6
viviblasco <viviblasco@hotmail.com>
2020-08-02 11:33:02 -0300

    Update Paciente.ts

--- POO/TP_Objetos/Paciente.ts
+++ POO/TP_Objetos/Paciente.ts
@@ -1,16 +1,52 @@
 class Paciente {
+   //variables
+    private idPaciente:number;
+    private DNI: number;
     private nombreYApellido:string;
     private edad:number;
     private telefono:number;
-
-    constructor(nombreYApellido:string,edad:number,telefono:number) {
+    
+   //constructor
+    constructor(idPaciente:number,DNI:number,nombreYApellido:string,edad:number,telefono:number) {
+       this.idPaciente=idPaciente; 
+       this.DNI=DNI; 
        this.nombreYApellido=nombreYApellido;
        this.edad = edad;
        this.telefono = telefono;
+       
     }
+   //metodos get
 
-    /*decirMiNombre() {
-       console.log(this.nombre);
-    }*/
+   public getId(): number {
+      return this.idPaciente;
+  }
+  public getDNI(): number {
+   return this.DNI;
+}
+  public getNombreYApellido(): string {
+      return this.nombreYApellido;
+  }
+  public getEdad(): number {
+      return this.edad;
+  } 
+  
+  public getTelefono(): number {
+      return this.telefono;
+  }
 
+
+  //metodos set
+   
+  public setId(idPaciente:number): void {
+         this.idPaciente=idPaciente;
+}
+  public setDNI(DNI:number):void{
+        this.DNI=DNI;
+  }
+  public setNombreYApellido(nombreYApellido:string):void{
+         this.nombreYApellido=nombreYApellido;
+  }
+  public setTelefono(telefono:number):void{
+         this.telefono=telefono;
+  }
 }
