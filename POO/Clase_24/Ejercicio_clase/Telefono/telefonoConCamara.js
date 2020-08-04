f5e39320155ff6c5bf3fadfff87cfa51f6b2fb94
viviblasco <viviblasco@hotmail.com>
2020-08-03 22:05:38 -0300

    Create telefonoConCamara.js

---
+++ POO/Clase_24/Ejercicio_clase/Telefono/telefonoConCamara.js
@@ -0,0 +1,27 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+exports.__esModule = true;
+var Telefono_1 = require("./Telefono");
+var TelefonoConCamara = /** @class */ (function (_super) {
+    __extends(TelefonoConCamara, _super);
+    function TelefonoConCamara() {
+        return _super.call(this) || this;
+    }
+    TelefonoConCamara.prototype.sacarFoto = function (mensaje) {
+        console.log("click");
+    };
+    return TelefonoConCamara;
+}(Telefono_1["default"]));
+exports["default"] = TelefonoConCamara;
