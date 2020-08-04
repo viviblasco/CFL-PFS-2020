0e8106d073a95452bcc22a37b8b237de15832b84
viviblasco <viviblasco@hotmail.com>
2020-08-03 22:05:27 -0300

    Create telefonoConRadio.js

---
+++ POO/Clase_24/Ejercicio_clase/Telefono/telefonoConRadio.js
@@ -0,0 +1,29 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+exports.__esModule = true;
+var Telefono_1 = require("./Telefono");
+var TelefonoConCamara = /** @class */ (function (_super) {
+    __extends(TelefonoConCamara, _super);
+    function TelefonoConCamara() {
+        var _this = _super.call(this) || this;
+        _this.frecuenciaActual = 85.5;
+        return _this;
+    }
+    TelefonoConCamara.prototype.verfrecuenciaActual = function (frecuenciaActual) {
+        console.log(frecuenciaActual);
+    };
+    return TelefonoConCamara;
+}(Telefono_1["default"]));
+exports["default"] = TelefonoConCamara;
