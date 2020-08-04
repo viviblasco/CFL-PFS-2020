ca5e4b35e700763cbce28380cd7866263f004c88
viviblasco <viviblasco@hotmail.com>
2020-08-03 22:08:51 -0300

    Create main.ts

---
+++ POO/Clase_24/Ejercicio_clase/Telefono/main.ts
@@ -0,0 +1,10 @@
+import TelefonoConCamara from "./telefonoConCamara";
+
+
+let movil =new TelefonoConCamara;
+
+public constructor(b: number) {
+
+    this.bateriaActual = bateriaInicial;
+    this.estaPrendido = false;
+}
