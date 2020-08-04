e044460bc3a4ca5eaebec0c68d11d3a636ce1b1f
viviblasco <viviblasco@hotmail.com>
2020-08-03 22:06:03 -0300

    Create telefonoConCamara.ts

---
+++ POO/Clase_24/Ejercicio_clase/Telefono/telefonoConCamara.ts
@@ -0,0 +1,13 @@
+import Telefono from "./Telefono";
+
+export default class TelefonoConCamara extends Telefono {
+    public constructor() {
+        super();
+ 
+    }
+
+public sacarFoto(mensaje:string):void{
+    console.log("click")
+
+ }
+} 
