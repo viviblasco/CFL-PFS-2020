f2c760b390eb37251d8c9b59af90a3e80b49d531
viviblasco <viviblasco@hotmail.com>
2020-08-07 19:56:34 -0300

    Update TelefonoConRadio.ts

--- POO/Clase_24/Ejercicio_clase/Telefono/TelefonoConRadio.ts
+++ POO/Clase_24/Ejercicio_clase/Telefono/TelefonoConRadio.ts
@@ -1,19 +1,18 @@
 
 import Telefono from "./Telefono";
 
-export default class TelefonoConCamara extends Telefono {
+export default class TelefonoConRadio extends Telefono {
 
-private frecuenciaActual:number;
+private frecuenciaActual:number =0;
 
 public constructor() {
     super();
-
     this.frecuenciaActual=85.5;
 }
 
 
 public  verfrecuenciaActual(frecuenciaActual:number):void{
-    console.log(frecuenciaActual)
+    console.log(frecuenciaActual);
 
  }
 }
