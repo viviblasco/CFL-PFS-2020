7a43a9644681e7815d4a33a25d7c2ea075451a1b
viviblasco <viviblasco@hotmail.com>
2020-08-07 20:50:40 -0300

    Create Camara.js

---
+++ POO/clase_25/telefono/Camara.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+var camara = /** @class */ (function () {
+    function camara() {
+    }
+    camara.prototype.sacarFotos = function () {
+        console.log("click");
+    };
+    return camara;
+}());
+exports["default"] = camara;
