13ed7608b81af0b0d98a34fc89db2e9d62911cb9
viviblasco <viviblasco@hotmail.com>
2020-08-07 20:50:55 -0300

    Create main.ts

---
+++ POO/clase_25/telefono/main.ts
@@ -0,0 +1,15 @@
+
+import Telefono from "./telefono";
+import TelefonoConCamara from "./TelefonoConCamara"
+
+
+
+let movil1:Telefono;
+let movil2:TelefonoConCamara;
+
+
+
+movil1.prenderApagar(false);
+movil2.prenderApagar(true);
+movil1.hacerLlamada;
+movil2.sacarFoto;
