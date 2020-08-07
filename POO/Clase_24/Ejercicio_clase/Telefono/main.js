8f74c5325ef8d9e174263023d0f6027160f75b82
viviblasco <viviblasco@hotmail.com>
2020-08-07 19:57:20 -0300

    Update main.js

--- POO/Clase_24/Ejercicio_clase/Telefono/main.js
+++ POO/Clase_24/Ejercicio_clase/Telefono/main.js
@@ -1,7 +1,8 @@
 "use strict";
 exports.__esModule = true;
-constructor(bateriaInicial, number);
-{
-    this.bateriaActual = bateriaInicial;
-    this.estaPrendido = false;
-}
+var telefonoConCamara_1 = require("./telefonoConCamara");
+var telefonoConRadio_1 = require("./telefonoConRadio");
+var movil = new telefonoConCamara_1["default"]();
+var movil2 = new telefonoConRadio_1["default"]();
+console.log(movil);
+console.log(movil2);
