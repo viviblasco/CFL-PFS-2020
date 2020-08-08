74a127b9c725ddaa3115cc97e3fe59214493950b
viviblasco <viviblasco@hotmail.com>
2020-08-07 21:49:17 -0300

    Update main.ts

--- POO/clase_25/telefono/main.ts
+++ POO/clase_25/telefono/main.ts
@@ -1,18 +1,26 @@
-
+//import * from "./node-emoji";
 import Telefono from "./telefono";
 import TelefonoConCamara from "./TelefonoConCamara"
 
 
+//declaracion de variables
 
 let movil1:Telefono;
 let movil2:TelefonoConCamara;
 
+//contrcutores
+movil1=new Telefono();
+movil2 =new TelefonoConCamara();
 
-movil1=new Telefono;
-movil2 =new TelefonoConCamara;
 
+//prender el Telefono
+movil1.prenderApagar(true);
+movil2.prenderApagar(true);
+
+movil2.mandarMensaje("Hola venis hoy al asado? Ya paso la Pandemia")
 movil1.hacerLlamada("hola",2494051223);
 
 
-console.log(movil2.sacarFoto);
+
+console.log(movil2.sacarFoto());
 
