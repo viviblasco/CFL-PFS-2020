b0c950ca4c13114cc7dad3252c2df923e216dab7
viviblasco <viviblasco@hotmail.com>
2020-08-07 21:48:59 -0300

    Update main.js

--- POO/clase_25/telefono/main.js
+++ POO/clase_25/telefono/main.js
@@ -1,10 +1,17 @@
 "use strict";
 exports.__esModule = true;
+//import * from "./node-emoji";
 var telefono_1 = require("./telefono");
 var TelefonoConCamara_1 = require("./TelefonoConCamara");
+//declaracion de variables
 var movil1;
 var movil2;
-movil1 = new telefono_1["default"];
-movil2 = new TelefonoConCamara_1["default"];
+//contrcutores
+movil1 = new telefono_1["default"]();
+movil2 = new TelefonoConCamara_1["default"]();
+//prender el Telefono
+movil1.prenderApagar(true);
+movil2.prenderApagar(true);
+movil2.mandarMensaje("Hola venis hoy al asado? Ya paso la Pandemia");
 movil1.hacerLlamada("hola", 2494051223);
-console.log(movil2.sacarFoto);
+console.log(movil2.sacarFoto());
