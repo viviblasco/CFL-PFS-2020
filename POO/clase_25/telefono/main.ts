aa98abeb76091a085be9981c83e75b72bb01208e
viviblasco <viviblasco@hotmail.com>
2020-08-07 21:15:31 -0300

    Update main.ts

--- POO/clase_25/telefono/main.ts
+++ POO/clase_25/telefono/main.ts
@@ -8,8 +8,11 @@ let movil1:Telefono;
 let movil2:TelefonoConCamara;
 
 
+movil1=new Telefono;
+movil2 =new TelefonoConCamara;
 
-movil1.prenderApagar(false);
-movil2.prenderApagar(true);
-movil1.hacerLlamada;
-movil2.sacarFoto;
+movil1.hacerLlamada("hola",2494051223);
+
+
+console.log(movil2.sacarFoto);
+
