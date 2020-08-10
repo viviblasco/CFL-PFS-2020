6832ad13a1a9506268aeca7089b1d879ac72ab50
viviblasco <viviblasco@hotmail.com>
2020-08-10 20:50:15 -0300

    Create main.js

---
+++ POO/clase_26/Auto/main.js
@@ -0,0 +1,6 @@
+var primerAuto = new Auto('Ford', 'Fiesta');
+var superAuto = new AutoDeportivo('Ford', 'Mustang');
+primerAuto.acelerar();
+superAuto.acelerar();
+console.log(primerAuto);
+console.log(superAuto);
