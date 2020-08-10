7abd4e5b19af914aa6314238d8f19c0354e33933
viviblasco <viviblasco@hotmail.com>
2020-08-10 20:50:45 -0300

    Create Auto.ts

---
+++ POO/clase_26/Auto/Auto.ts
@@ -0,0 +1,16 @@
+export class Auto {
+ private marca: string;
+ private modelo: string;
+ protected velocidadActual: number;
+
+
+ public constructor(marca: string, modelo: string) {
+ this.marca = marca;
+ this.modelo = modelo;
+ this.velocidadActual = 0;
+ }
+ 
+ public acelerar(): void {
+ this.velocidadActual += 10;
+ }
+}
