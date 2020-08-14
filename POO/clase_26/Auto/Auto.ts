063df7b8dda0e5acfbbb1333c7d56506ce546e57
viviblasco <viviblasco@hotmail.com>
2020-08-14 20:17:23 -0300

    Update Auto.ts

--- POO/clase_26/Auto/Auto.ts
+++ POO/clase_26/Auto/Auto.ts
@@ -1,16 +1,16 @@
-export class Auto {
- private marca: string;
- private modelo: string;
- protected velocidadActual: number;
 
+export default class Auto {
+    private marca: string; 
+    private modelo: string; 
+    protected velocidadActual: number;
+    public constructor(marca: string, modelo: string) { 
+        this.marca = marca; 
+        this.modelo = modelo; 
+        this.velocidadActual = 0;
+    }
 
- public constructor(marca: string, modelo: string) {
- this.marca = marca;
- this.modelo = modelo;
- this.velocidadActual = 0;
- }
- 
- public acelerar(): void {
- this.velocidadActual += 10;
- }
+    public acelerar(): void { 
+        this.velocidadActual += 10;
+    }    
+
 }
